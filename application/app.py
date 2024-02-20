ac2054d0698ea4ae91e9e6a3a2964bab90695b68
Wiktor Piela <wpiela9@gmail.com>
2024-02-20 15:42:27 +0100

    backend new testing views

--- application/app.py
+++ application/app.py
@@ -1,19 +1,43 @@
 from flask import Flask, jsonify
-from flask.views import MethodView
+from flask_restful import Resource, Api
 from flask_cors import CORS
 
 app = Flask(__name__)
+api = Api(app)
 CORS(app)
 
-# Define a class-based view
-class TestView(MethodView):
+books = []
+
+class TestView(Resource):
     def get(self):
-        for i in range(10):
-            print(i)
         return jsonify({'message':'hellow world'})
 
-# Register the BooksAPI class as a view
-app.add_url_rule('/', view_func=TestView.as_view('test'))
+class BookView(Resource):
+
+    def get(self, title):
+        for book in books:
+            if book['title'] == title:
+                return book
+        return {'title': None}
+    def post(self, title):
+        book = {'name': title}
+        books.append(book)
+        return book
+    def delete(self, title):
+        for index, book, in enumerate(books):
+            if book['title'] == title:
+                deleted_book = books.pop(index)
+                return {'note': 'delete success'}
+        return {'note': 'delete unsuccessful'}
+
+class AllNames(Resource):
+    def get(self):
+        return {'books': books}
+
+api.add_resource(TestView, '/')
+api.add_resource(AllNames, '/all-names/')
+api.add_resource(BookView, '/books/')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
