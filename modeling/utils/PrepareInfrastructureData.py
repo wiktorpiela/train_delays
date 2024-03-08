a12bec0f81d92fa2b56872619d05dae8f13cd7e7
Wiktor Piela <wpiela9@gmail.com>
2024-03-08 08:47:18 +0100

    prepare railway infrastructure data helper functions

---
+++ modeling/utils/PrepareInfrastructureData.py
@@ -0,0 +1,69 @@
+import osmnx
+from shapely.geometry import Polygon, LineString, Point
+import geopandas as gpd
+from typing import List
+import numpy as np
+
+def measure_linestring_distance_inside_polygon(routes_list:List, polygon):
+    intersection_lin_str = []
+    for route in routes_list:
+        inter = route.intersection(polygon)
+        intersection_lin_str.append(inter)
+        
+    route_gdf = gpd.GeoDataFrame(geometry=intersection_lin_str)
+    route_gdf.crs = "EPSG:4326"
+    route_gdf_projected = route_gdf.to_crs("EPSG:32610")
+    total_distance_km = route_gdf_projected.geometry.length.sum() / 1000
+    return total_distance_km
+    
+
+def get_infrastrucute_data_per_area(path_to_shp_file:str, path_to_save_output_file:str):
+    shp_file = gpd.read_file(path_to_shp_file, encoding='utf-8')
+    #areas_list = shp_file['JPT_NAZWA_'].unique()
+    areas_list = shp_file['JPT_KOD_JE'].unique()
+    polygon_list = []
+    switches_list = []
+    level_crossing_list = []
+    stations_list = []
+    distances_km_list = []
+    for area in areas_list:
+        polygon = shp_file[shp_file['JPT_KOD_JE']==area]['geometry'].item()
+
+        try:
+            railway_features = osmnx.geometries_from_polygon(polygon, tags={'railway': True}).reset_index()
+
+            if 'ref' in railway_features.columns:
+                routes = railway_features[(railway_features['element_type']=='way') & (railway_features['railway']=='rail') & (~railway_features['ref'].isna())]['geometry'].tolist()
+                switches = railway_features[(railway_features['element_type']=='node') & (railway_features['railway']=='switch') & (~railway_features['ref'].isna())]['geometry'].tolist()
+                level_crossing = railway_features[(railway_features['element_type']=='node') & (railway_features['railway']=='level_crossing') & (~railway_features['ref'].isna())]['geometry'].tolist()
+            else:
+                routes = railway_features[(railway_features['element_type']=='way') & (railway_features['railway']=='rail')]['geometry'].tolist()
+                switches = railway_features[(railway_features['element_type']=='node') & (railway_features['railway']=='switch')]['geometry'].tolist()
+                level_crossing = railway_features[(railway_features['element_type']=='node') & (railway_features['railway']=='level_crossing')]['geometry'].tolist()
+                
+            stations = railway_features[(railway_features['element_type']=='node') & (railway_features['railway'].isin(['station', 'halt']))]['geometry'].tolist()
+            distance_km = measure_linestring_distance_inside_polygon(routes, polygon)
+            
+        except ValueError as e:
+            switches = []
+            level_crossing = [] 
+            stations = []
+            distance_km = np.nan
+            print(e)
+            
+        polygon_list.append(polygon)
+        switches_list.append(len(switches))
+        level_crossing_list.append(len(level_crossing))
+        stations_list.append(len(stations))
+        distances_km_list.append(distance_km)
+
+    gpd_df_out = gpd.GeoDataFrame({
+        'area': areas_list,
+        'polygon': polygon_list,
+        'switches_count': switches_list,
+        'level_crossing_count': level_crossing_list,
+        'stations_count': stations_list,
+        'distance': distances_km_list
+    })
+
+    gpd_df_out.to_file(path_to_save_output_file)
