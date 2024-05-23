e1039d1cdf125909fd4404db003ac05f8b560a92
Wiktor Piela <wpiela9@gmail.com>
2024-05-23 15:51:32 +0200

    timetable data prepare script updated

--- modeling/03h_helpers_timetable_data_to_db.py
+++ modeling/03h_helpers_timetable_data_to_db.py
@@ -8,6 +8,15 @@ timetable['arrival_time'] = timetable['arrival_on_time'].dt.time
 timetable['departure_time'] = timetable['departure_on_time'].dt.time
 timetable  = timetable[['key','Stacja','arrival_time', 'departure_time']].drop_duplicates()
 
+keys = timetable['key'].unique()
+timetables = []
+for key in keys:
+    curr_timetable = timetable[timetable['key']==key].copy()
+    curr_timetable = curr_timetable.drop_duplicates(subset=['Stacja'], keep='first')
+    timetables.append(curr_timetable)
+
+final_timetable = pd.concat(timetables, ignore_index=True)
+
 stations = gpd.read_file('../data/spatial_data/stations_gps_crs_4326_utf8.shp')
 stations['station_id'] = stations.index + 1
 
@@ -15,8 +24,6 @@ rel = pd.read_parquet('../data/routes_data_complete_1804.parquet')
 rel = rel[['key']].drop_duplicates().reset_index(drop=True)
 rel['route_id'] = rel.index + 1
 
-timetable = timetable.merge(rel, how='left', on='key').merge(stations.drop('geometry', axis=1), how='left', on='Stacja')
-
-timetable_final = timetable[['route_id', 'station_id', 'arrival_time', 'departure_time']]
-
-timetable_final.to_csv('../data/timetable_data_to_db.csv', index=False)
+final_timetable = final_timetable.merge(rel, how='left', on='key').merge(stations.drop('geometry', axis=1), how='left', on='Stacja')
+final_timetable = final_timetable[['route_id', 'station_id', 'arrival_time', 'departure_time']]
+final_timetable.to_csv('../data/timetable_data_to_db__2305.csv', index=False)
